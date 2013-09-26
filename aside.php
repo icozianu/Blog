7d58ecfd0da2d55ea5c243b6dc17d414fd4e910b
claudiumstoica <claudiu.stoica@sendgrid.com>
2013-09-26 09:38:45 +0300

    Sign Up form

--- aside.php
+++ aside.php
@@ -4,18 +4,20 @@
 	<!--the box where the user can enter the credentials-->
 	<div class="form-horizontal well">
 		<p style="font-size:22px">Login</p>
-		<form  method="POST" action="signup.php"/>
+		<form  action="signin.php" method="POST">
 		Username<br/><input  type="text" name="username" value=""/><br/>
 		Password<br/><input type="password" name="password" value=""/><br/>
 		<input type="checkbox" name="remember" value=""> Remember me<br/>
 		<input id="buttons" class="button btn-primary" type="submit" name="submit" value="Login"/>
+		</form>
 	</div>
 	
 	<!--the box for creating new accounts-->
 	<div class="form-horizontal well">
 		<p style="font-size:18px">Create a New Account</p>
-		<form action="signup.php" method="POST"/>
-		<input class="button btn-success" type="submit" value="Submit"/>
+		<form action="signup.php" method="POST">
+		<input class="button btn-success" type="submit" value="Sign Up"/>
+		</form>
 	</div>
 
 	<!--for short memory persons who forget their login credentials-->
---
+++ register.php
@@ -0,0 +1,13 @@
+<p style="font-size:22px">Register</p>
+<form style="font-size: 14px" action="signin.php" method="POST">
+	<ul>
+		<li>Username*</br><input  type="text" name="username" size="50"><br/><br/></li>
+		<li>Password*</br><input type="password" name="password" size="50"><br/><br/></li>
+		<li>Repeat Password*</br><input type="password" name="password_again" size="50"><br/><br/></li>
+		<li>First Name*</br><input type="text" name="first_name" size="50"><br/><br/></li>
+		<li>Last Name</br><input type="text" name="last_name" size="50"><br/><br/></li>
+		<li>E-mail Address*</br><input type="email" name="password" size="50"><br/><br/></li>
+		<li><input id="buttons" class="button btn-primary" type="submit" value="Create Account"/></li>
+	</ul>
+		
+</form>
